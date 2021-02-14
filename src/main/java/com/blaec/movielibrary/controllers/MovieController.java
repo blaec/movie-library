73ee415c32fe38b99eb32864e271046103d0de12
blaec <odeskonst@yandex.ru>
2021-02-14 21:30:34 +0200

    sort movies

--- src/main/java/com/blaec/movielibrary/controllers/MovieController.java
+++ src/main/java/com/blaec/movielibrary/controllers/MovieController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Slf4j
 @AllArgsConstructor
 @RequestMapping("/movies")
@@ -17,6 +21,24 @@ public class MovieController {
 
     @GetMapping
     public Iterable<Movie> getAll() {
-        return movieRepository.findAll();
+        return sortByTitleAndYear(movieRepository.findAll());
+    }
+
+    /**
+     * Sort movie list by title and than by release date, skip 'the' and 'a' in title
+     *
+     * @param movies list of movies to sort
+     * @return sorted list
+     */
+    private Iterable<Movie> sortByTitleAndYear(Iterable<Movie> movies) {
+        return StreamSupport.stream(movies.spliterator(), false).sorted(Comparator
+                .comparing((Movie m) ->
+                        m.getTitle().startsWith("The ")
+                            ? m.getTitle().replace("The ", "")
+                            : m.getTitle().startsWith("A ")
+                                ? m.getTitle().replace("A ", "")
+                                : m.getTitle())
+                .thenComparing(Movie::getReleaseDate))
+                .collect(Collectors.toList());
     }
 }
