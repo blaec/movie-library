6f967a06a25f1c7cc50a2a873eec52e1941ee0ad
blaec <odeskonst@yandex.ru>
2021-02-15 19:36:05 +0200

    #1 fetch request

--- src/main/java/com/blaec/movielibrary/controllers/MovieController.java
+++ src/main/java/com/blaec/movielibrary/controllers/MovieController.java
@@ -4,6 +4,7 @@ import com.blaec.movielibrary.model.Movie;
 import com.blaec.movielibrary.repository.MovieRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.stream.StreamSupport;
 public class MovieController {
     private final MovieRepository movieRepository;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public Iterable<Movie> getAll() {
         return sortByTitleAndYear(movieRepository.findAll());
