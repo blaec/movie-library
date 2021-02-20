41918b073c2ca130825b54bf5fd5fd385805df75
blaec <odeskonst@yandex.ru>
2021-02-20 16:16:54 +0200

    #9 create form

--- movies/src/component/Upload/Upload.js
+++ movies/src/component/Upload/Upload.js
@@ -3,24 +3,39 @@ import './Upload.css';
 import {
     Button,
     Card, CardActions,
-    CardContent,
+    CardContent, Divider,
     FormControl,
     FormControlLabel,
-    FormLabel,
+    FormLabel, Grid,
     Radio,
-    RadioGroup, TextField
+    RadioGroup, Switch, TextField, Typography
 } from "@material-ui/core";
 import BackupTwoToneIcon from '@material-ui/icons/BackupTwoTone';
 import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
 import axios from '../../axios-movies';
+import * as PropTypes from "prop-types";
 
+class AntSwitch extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+AntSwitch.propTypes = {
+    name: PropTypes.string,
+    onChange: PropTypes.func,
+    checked: PropTypes.any
+};
 const upload = props => {
-    const [scanValue, setScanValue] = useState('cartoons');
+    const [scanValue, setScanValue] = useState('');
     const [wishValue, setWishValue] = useState('cartoons');
     const [loading, setLoading] = useState(false);
+    const [isSingle, setIsManual] = useState(false);
+    const [canUpload, setCanUpload] = useState(false);
 
     const handleChange = (event) => {
         setScanValue(event.target.value);
+        setCanUpload(true);
     };
 
     const handleUpload = () => {
@@ -28,34 +43,67 @@ const upload = props => {
         axios.post(`/movies/${scanValue}`)
             .then(response => {
                 setLoading(false);
+                setCanUpload(false);
+                setScanValue('');
                 alert(`uploading from ${scanValue} folder completed successfully.`)
             })
             .catch(error => {
                 setLoading(false);
+                setCanUpload(false);
+                setScanValue('');
                 console.log(error);
             });
 
     };
 
+    const handleIsSingle = () => {
+        setIsManual(!isSingle);
+    }
+
     return (
         <div className="Upload">
             <Card variant="elevation">
                 <CardContent>
-                    <FormControl>
-                        <FormLabel >Movie location</FormLabel>
+                    <FormControl component="locations">
+                        <FormLabel component="ssll">Movie location</FormLabel>
                         <RadioGroup name="location" value={scanValue} onChange={handleChange}>
                             <FormControlLabel value="cartoons" control={<Radio />} label="Cartoons" />
                             <FormControlLabel value="movies" control={<Radio />} label="Movies" />
                             <FormControlLabel value="serialMovies" control={<Radio />} label="Serial Movies" />
-                            <FormControlLabel value="music" control={<Radio />} label="Music" />
+                            <FormControlLabel value="music" control={<Radio />} label="New Movies" />
                             <FormControlLabel value="videos" control={<Radio />} label="Videos" />
                         </RadioGroup>
+                    </FormControl>
+                    <Divider />
+                    <FormControl component="single-upload">
+                        <FormLabel component="legend">Single Movie Upload</FormLabel>
+                        <FormControlLabel
+                            control={<Switch color="primary" checked={isSingle} onChange={handleIsSingle} name="isSingle" />}
+                            label="Single movie upload"
+                        />
+                        <TextField id="tmdb-id"
+                                   disabled={!isSingle}
+                                   label="tmdb id"
+                                   style={{margin: 8}}
+                                   helperText="Type exact tmdb id"
+                                   fullWidth
+                                   margin="normal"
+                        />
+                        <TextField id="file-name"
+                                   disabled={!isSingle}
+                                   label="Exact file name"
+                                   style={{margin: 8}}
+                                   helperText="Enter exact file name with extension"
+                                   fullWidth
+                                   margin="normal"
+                        />
                     </FormControl>
                 </CardContent>
                 <CardActions>
                     <Button className="Button"
+                            disabled={!canUpload}
                             variant="outlined"
-                            color="secondary"
+                            color="primary"
                             startIcon={<BackupTwoToneIcon />}
                             onClick={handleUpload}>
                         Scan
@@ -69,7 +117,7 @@ const upload = props => {
                         <FormLabel >Add to Wish List</FormLabel>
                         <TextField
                             id="standard-full-width"
-                            color="secondary"
+                            color="primary"
                             label="Label"
                             style={{ margin: 8 }}
                             placeholder="Placeholder"
@@ -85,7 +133,7 @@ const upload = props => {
                 <CardActions >
                     <Button
                             variant="outlined"
-                            color="secondary"
+                            color="primary"
                             startIcon={<AddCircleTwoToneIcon />}
                             onClick={handleUpload}>
                         Add
