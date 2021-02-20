65bc97812a6ad1c843a667b70b16437237b0ca2e
blaec <odeskonst@yandex.ru>
2021-02-20 16:43:20 +0200

    #9 restyling

--- movies/src/component/Upload/Upload.js
+++ movies/src/component/Upload/Upload.js
@@ -2,31 +2,33 @@ import React, {useState} from 'react';
 import './Upload.css';
 import {
     Button,
-    Card, CardActions,
-    CardContent, Divider,
+    Card,
+    CardActions,
+    CardContent,
+    Divider,
     FormControl,
     FormControlLabel,
-    FormLabel, Grid,
+    FormLabel, makeStyles,
     Radio,
-    RadioGroup, Switch, TextField, Typography
+    RadioGroup,
+    Switch,
+    TextField
 } from "@material-ui/core";
 import BackupTwoToneIcon from '@material-ui/icons/BackupTwoTone';
 import AddCircleTwoToneIcon from '@material-ui/icons/AddCircleTwoTone';
 import axios from '../../axios-movies';
-import * as PropTypes from "prop-types";
 
-class AntSwitch extends React.Component {
-    render() {
-        return null;
+const useStyles = makeStyles((theme) => ({
+    divider: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(2),
     }
-}
+}));
 
-AntSwitch.propTypes = {
-    name: PropTypes.string,
-    onChange: PropTypes.func,
-    checked: PropTypes.any
-};
+
 const upload = props => {
+    const classes = useStyles();
+
     const [scanValue, setScanValue] = useState('');
     const [wishValue, setWishValue] = useState('cartoons');
     const [loading, setLoading] = useState(false);
@@ -65,21 +67,35 @@ const upload = props => {
             <Card variant="elevation">
                 <CardContent>
                     <FormControl component="locations">
-                        <FormLabel component="ssll">Movie location</FormLabel>
-                        <RadioGroup name="location" value={scanValue} onChange={handleChange}>
-                            <FormControlLabel value="cartoons" control={<Radio />} label="Cartoons" />
-                            <FormControlLabel value="movies" control={<Radio />} label="Movies" />
-                            <FormControlLabel value="serialMovies" control={<Radio />} label="Serial Movies" />
-                            <FormControlLabel value="music" control={<Radio />} label="New Movies" />
-                            <FormControlLabel value="videos" control={<Radio />} label="Videos" />
+                        <FormLabel>Movie location</FormLabel>
+                        <RadioGroup name="location"
+                                    value={scanValue}
+                                    onChange={handleChange}>
+                            <FormControlLabel value="cartoons"
+                                              control={<Radio color="primary" />}
+                                              label="Cartoons" />
+                            <FormControlLabel value="movies"
+                                              control={<Radio color="primary" />}
+                                              label="Movies" />
+                            <FormControlLabel value="serialMovies"
+                                              control={<Radio color="primary" />}
+                                              label="Serial Movies" />
+                            <FormControlLabel value="music"
+                                              control={<Radio color="primary" />}
+                                              label="New Movies" />
+                            <FormControlLabel value="videos"
+                                              control={<Radio color="primary" />}
+                                              label="Videos" />
                         </RadioGroup>
                     </FormControl>
-                    <Divider />
+                    <Divider  className={classes.divider}/>
                     <FormControl component="single-upload">
                         <FormLabel component="legend">Single Movie Upload</FormLabel>
-                        <FormControlLabel
-                            control={<Switch color="primary" checked={isSingle} onChange={handleIsSingle} name="isSingle" />}
-                            label="Single movie upload"
+                        <FormControlLabel label="Single movie upload"
+                                          control={<Switch color="primary"
+                                                           checked={isSingle}
+                                                           onChange={handleIsSingle}
+                                                           name="isSingle"/>}
                         />
                         <TextField id="tmdb-id"
                                    disabled={!isSingle}
@@ -131,8 +147,7 @@ const upload = props => {
                     </FormControl>
                 </CardContent>
                 <CardActions >
-                    <Button
-                            variant="outlined"
+                    <Button variant="outlined"
                             color="primary"
                             startIcon={<AddCircleTwoToneIcon />}
                             onClick={handleUpload}>
