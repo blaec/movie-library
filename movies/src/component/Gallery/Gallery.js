a9b8a06bb72c740faaecb6ed3f59f305d0ea27c3
blaec <odeskonst@yandex.ru>
2021-02-27 12:41:06 +0200

    #11 Check lazy-loading options

--- movies/src/component/Gallery/Gallery.js
+++ movies/src/component/Gallery/Gallery.js
@@ -4,6 +4,7 @@ import "./Gallery.css";
 import axios from "../../axios-movies";
 import Details from "../Details/Details";
 import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import Pagination from '@material-ui/lab/Pagination';
 
 const gallery = () => {
     const [movieList, setMovieList] = useState([]);
@@ -11,7 +12,28 @@ const gallery = () => {
     const [selected, setSelected] = useState(false);
     const [scrollY, setScrollY] = useState();
     const [deletePrompt, setDeletePrompt] = useState(false);
-    const [deleteId, setDeleteId] = useState(0);
+    const [deleteId, setDeleteId] = useState();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [moviesPerPage, setMoviesPerPage] = useState(0);
+    const [movieCount, setMovieCount] = useState(0);
+
+    let innerWidth = window.innerWidth;
+    useEffect(() => {
+        console.log("width: " + moviesPerPage);
+        let rows = 3;
+        let moviesPerRow = 7;
+        switch (true) {
+            case (innerWidth < 400):    rows = 12;  moviesPerRow = 2;   break;
+            case (innerWidth <= 700):   rows = 7;   moviesPerRow = 3;   break;
+            case (innerWidth <= 1000):  rows = 6;   moviesPerRow = 4;   break;
+            case (innerWidth <= 1300):  rows = 5;   moviesPerRow = 5;   break;
+            case (innerWidth <= 1700):  rows = 4;   moviesPerRow = 6;   break;
+        }
+        setMoviesPerPage(rows * moviesPerRow);
+        setMovieCount(Math.ceil(movieList.length / moviesPerPage));
+
+        }, [movieList]
+    );
 
     const handlerSelectCard = (movie) => {
         setScrollY(window.scrollY);
@@ -50,7 +72,12 @@ const gallery = () => {
             });
     };
 
+    const handlePagination = (event, page) => {
+        setCurrentPage(page);
+    };
+
     useEffect(() => {
+        console.log("get: " + moviesPerPage);
         axios.get('/movies')
             .then(response => {
                 setMovieList(response.data);
@@ -66,17 +93,31 @@ const gallery = () => {
         }
     }, [selected, scrollY]);
 
+    const indexOfLastMovie = currentPage * moviesPerPage;
+    const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+    const currentMovies = movieList.slice(indexOfFirstMovie, indexOfLastMovie);
+
     let myGallery = selected
         ? (<Details closed={handlerClose}
                     delete={handleDelete}
                     {...selectedCard}/>)
-        : (<div className="Gallery">
-            {movieList.map(m =>
-                <Card key={m.id}
-                      {...m}
-                      clicked={handlerSelectCard}/>
-            )}
-           </div>);
+        : ( <React.Fragment>
+                <div className="Gallery">
+                    {currentMovies.map(m =>
+                        <Card key={m.id}
+                              {...m}
+                              clicked={handlerSelectCard}/>
+                    )}
+                </div>
+                <Pagination className="Pagination"
+                            page={currentPage}
+                            count={movieCount}
+                            onChange={handlePagination}
+                            showFirstButton
+                            showLastButton
+                            variant="outlined"
+                            color="primary"/>
+            </React.Fragment>);
 
     return (
         <React.Fragment>
