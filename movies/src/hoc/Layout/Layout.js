bb53bd9f1a5d4ff9ded50490bfaa2fa413f99bd3
blaec <odeskonst@yandex.ru>
2021-02-28 22:46:42 +0200

    #14 clean code - remove unused code

--- movies/src/hoc/Layout/Layout.js
+++ movies/src/hoc/Layout/Layout.js
@@ -1,26 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Layout.css';
 import Toolbar from "../../component/Navigation/Toolbar/Toolbar";
-import SideDrawer from "../../component/Navigation/SideDrawer/SideDrawer";
 
 const layout = props => {
-    const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
-
-    const sideDrawerClosedHandler = () => {
-        setSideDrawerIsVisible(false);
-        console.log(`sideDrawerClosedHandler - sideDrawerIsVisible ${sideDrawerIsVisible}`)
-    };
-
-    const sideDrawerToggleHandler = () => {
-        setSideDrawerIsVisible(!sideDrawerIsVisible);
-        console.log(`sideDrawerToggleHandler - sideDrawerIsVisible ${sideDrawerIsVisible}`)
-    };
-
     return (
         <React.Fragment>
-            <Toolbar drawerToggleClicked={sideDrawerToggleHandler}/>
-            {/*<SideDrawer open={sideDrawerIsVisible}*/}
-            {/*            closed={sideDrawerClosedHandler}/>*/}
+            <Toolbar />
             <main className="Children">
                 {props.children}
             </main>
