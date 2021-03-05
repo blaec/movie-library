3c1b183eaf74b7e50f2574fc3243f784a2c14a0d
blaec <odeskonst@yandex.ru>
2021-03-05 23:12:45 +0200

    #16 extract drawer

--- movies/src/component/Toolbar/Toolbar.js
+++ movies/src/component/Toolbar/Toolbar.js
@@ -1,39 +1,20 @@
 import React, {useState} from 'react';
 
-import MovieMenu from "./components/MovieMenu";
 import MyAppBar from "./components/MyAppBar";
-import './Toolbar.css'
+import MyDrawer from "./components/MyDrawer";
 
-import {CssBaseline, Divider, Drawer, fade, Hidden} from "@material-ui/core";
-import {makeStyles, useTheme} from '@material-ui/core/styles';
-import {DRAWER_WIDTH} from "../../utils/Constants";
+import {CssBaseline} from "@material-ui/core";
+import {makeStyles} from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: DRAWER_WIDTH,
-            flexShrink: 0,
-        },
-    },
-    // necessary for content to be below app bar
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-        width: DRAWER_WIDTH,
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
     }
 }));
 
 const toolbar = props => {
-    const {window} = props;
     const classes = useStyles();
-    const theme = useTheme();
     const [mobileOpen, setMobileOpen] = useState(false);
 
     const handleDrawerToggle = () => {
@@ -44,19 +25,7 @@ const toolbar = props => {
         if (mobileOpen) {
             setMobileOpen(false);
         }
-    }
-
-    const drawer = (
-        <div>
-            <div className={classes.toolbar}/>
-            <Divider/>
-            <MovieMenu clicked={handleMobileMenuClose}/>
-        </div>
-    );
-
-    const container = window !== undefined
-        ? () => window().document.body
-        : undefined;
+    };
 
     return (
         <div className={classes.root}>
@@ -64,39 +33,13 @@ const toolbar = props => {
             <MyAppBar classes={classes}
                       toggle={handleDrawerToggle}
             />
-            <nav className={classes.drawer} aria-label="mailbox folders">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        variant="temporary"
-                        open={mobileOpen}
-                        container={container}
-                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                        onClose={handleDrawerToggle}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        variant="permanent"
-                        open
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-            </nav>
+            <MyDrawer {...props}
+                      mobileClose={handleMobileMenuClose}
+                      drawerToggle={handleDrawerToggle}
+                      open={mobileOpen}
+            />
         </div>
     );
-};
+}
 
 export default toolbar;
---
+++ movies/src/component/Toolbar/components/MyDrawer.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import MovieMenu from "./MovieMenu";
+import {DRAWER_WIDTH} from "../../../utils/Constants";
+
+import {Divider, Drawer, Hidden} from "@material-ui/core";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: DRAWER_WIDTH,
+            flexShrink: 0,
+        },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: DRAWER_WIDTH,
+    }
+}));
+
+
+const myDrawer = (props) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const {window} = props;
+
+    const drawer = (
+        <div>
+            <div className={classes.toolbar}/>
+            <Divider/>
+            <MovieMenu clicked={props.mobileClose}/>
+        </div>
+    );
+
+    const container = window !== undefined
+        ? () => window().document.body
+        : undefined;
+
+    return (
+        <nav className={classes.drawer} aria-label="mailbox folders">
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Hidden smUp implementation="css">
+                <Drawer
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    variant="temporary"
+                    open={props.open}
+                    container={container}
+                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                    onClose={props.drawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+                <Drawer
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    variant="permanent"
+                    open
+                >
+                    {drawer}
+                </Drawer>
+            </Hidden>
+        </nav>
+    );
+};
+
+export default myDrawer;
