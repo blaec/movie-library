c5814411fcfb1d70fc42ac7d6a9f501ef69a1cd5
blaec <odeskonst@yandex.ru>
2021-03-12 22:26:27 +0200

    #19 display all found movies using carousel

--- movies/src/component/Pages/Upload/components/WishLoader.js
+++ movies/src/component/Pages/Upload/components/WishLoader.js
@@ -2,17 +2,31 @@ import React from 'react';
 import MyTextField from "../../../../UI/MyTextField";
 import MySubmitButton from "../../../../UI/Buttons/MySubmitButton";
 
-import Movie from "../../Gallery/Gallery/components/Movie/Movie";
-
 import {Card, CardActions, CardContent, FormControl, FormLabel, LinearProgress} from "@material-ui/core";
 import AddCircleTwoToneIcon from "@material-ui/icons/AddCircleTwoTone";
 import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
+import Carousel from "react-material-ui-carousel";
+import {getImageUrl} from "../../../../utils/UrlUtils";
 
 const wishLoader = props => {
     let movie = null;
     if (props.wishResults) {
-        movie = <Movie posterPath={props.wishResults[0].poster_path}
-                       releaseDate={props.wishResults[0].release_date}/>
+        movie = <Carousel animation="slide"
+                          autoPlay={false}
+                          navButtonsAlwaysVisible>
+                    {props.wishResults.map((poster, idx) => {
+                            return <div>
+                                        <p>{poster.title}</p>
+                                        <img key={idx}
+                                             height={200}
+                                             onError={(e)=>{e.target.onerror = null; e.target.src="https://via.placeholder.com/200x300.png?text=NOT FOUND"}}
+                                             src={getImageUrl(poster.poster_path)}
+                                             alt={props.alt}
+                                        />
+                                    </div>;
+                        }
+                    )}
+                </Carousel>
     }
     return (
         <Card variant="elevation">
