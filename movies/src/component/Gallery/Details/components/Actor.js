5d2b36e5799c368612a519caa054d1582b09b1b3
blaec <odeskonst@yandex.ru>
2021-03-16 22:13:23 +0200

    #13 enlarge actor avatar

--- movies/src/component/Gallery/Details/components/Actor.js
+++ movies/src/component/Gallery/Details/components/Actor.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import {Avatar, Divider, ListItem, ListItemAvatar, ListItemText, Typography} from "@material-ui/core";
+import {Avatar, Divider, ListItem, ListItemAvatar, ListItemText, makeStyles, Typography} from "@material-ui/core";
 import {getImageUrl} from "../../../../utils/UrlUtils";
 
+const useStyles = makeStyles((theme) => ({
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+    },
+    actor: {
+        paddingLeft: theme.spacing(2),
+    }
+}));
+
 const actor = (props) => {
+    const classes = useStyles();
+
     return (
         <React.Fragment>
             <ListItem alignItems="flex-start">
                 <ListItemAvatar>
-                    <Avatar variant="rounded" alt={props.name} src={getImageUrl(props.profile_path)}/>
+                    <Avatar variant="rounded"
+                            alt={props.name}
+                            src={getImageUrl(props.profile_path)}
+                            className={classes.large}
+                    />
                 </ListItemAvatar>
                 <ListItemText
+                    className={classes.actor}
                     primary={props.name}
                     secondary={" — as " + props.character}
                 />
