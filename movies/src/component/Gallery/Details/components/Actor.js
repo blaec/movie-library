09a5bd4422ac16ff5d63f4ab12056e973af7561e
blaec <odeskonst@yandex.ru>
2021-03-16 23:12:10 +0200

    #13 Add actors to details

---
+++ movies/src/component/Gallery/Details/components/Actor.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Avatar, ListItem, ListItemAvatar, ListItemText, makeStyles} from "@material-ui/core";
+import {getImageUrl} from "../../../../utils/UrlUtils";
+
+const useStyles = makeStyles((theme) => ({
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+    },
+    actor: {
+        paddingLeft: theme.spacing(2),
+    }
+}));
+
+const actor = (props) => {
+    const classes = useStyles();
+
+    return (
+        <ListItem>
+            <ListItemAvatar>
+                <Avatar variant="rounded"
+                        alt={props.name}
+                        src={getImageUrl(props.profile_path)}
+                        className={classes.large}
+                />
+            </ListItemAvatar>
+            <ListItemText className={classes.actor}
+                          primary={props.name}
+                          secondary={` — as ${props.character}`}
+            />
+        </ListItem>
+    );
+}
+
+export default actor;
--- movies/src/component/Gallery/Details/components/Info.js
+++ movies/src/component/Gallery/Details/components/Info.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-import {Divider, Typography} from "@material-ui/core";
+import {Box, Divider, List, makeStyles, Paper, Tab, Tabs, Typography} from "@material-ui/core";
 
-import {playTime, year} from "../../../../utils/Utils";
+import {a11yProps, playTime, year} from "../../../../utils/Utils";
+import Actor from "./Actor";
 import '../Details.css';
+import TabPanel from "../../../Tabs/TabPanel";
+import * as PropTypes from "prop-types";
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
 
 const info = props => {
+    console.log(props);
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     const metadata = {
         release_date: year(props.details.release_date),
         runtime: props.details.runtime !== 0
@@ -18,29 +42,67 @@ const info = props => {
     };
 
     return (
-        <div className="Info">
-            <Typography variant="caption" display="block" gutterBottom color="textSecondary">
-                {props.file.location}
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-                {Object.values(metadata)
-                    .filter(val => val !== null)
-                    .join(` \u2B24 `)}
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-                <strong>{props.details.title}</strong>
-            </Typography>
-            <Typography variant="subtitle2" gutterBottom>
-                {props.genres}
-            </Typography>
-            <Divider/>
-            <Typography variant="body1" gutterBottom>
-                <strong>{props.details.tagline}</strong>
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-                {props.details.overview}
-            </Typography>
-        </div>
+        <React.Fragment>
+
+            {/* Main movie info */}
+            <div className="Info">
+                <Typography component="div">
+                    <Box fontSize="caption.fontSize"
+                         fontWeight="fontWeightLight">
+                        {props.file.location}
+                    </Box>
+                    <Divider/>
+                    <Box fontSize="subtitle2.fontSize"
+                         fontWeight="fontWeightRegular"
+                         textAlign="center"
+                         paddingTop={1}
+                    >
+                        {Object.values(metadata)
+                            .filter(val => val !== null)
+                            .join(` | `)}
+                    </Box>
+                    <Box fontSize="h4.fontSize" fontWeight="fontWeightBold" textAlign="center">
+                        {props.details.title}
+                    </Box>
+                    <Box fontSize="subtitle2.fontSize"  fontWeight="fontWeightMedium" textAlign="center">
+                        {props.genres}
+                    </Box>
+                </Typography>
+            </div>
+
+            {/* Additional movie info: tabs: Info and Cast */}
+            <div className={classes.root}>
+                <Paper square>
+                    <Tabs value={value}
+                          onChange={handleChange}
+                          indicatorColor="primary"
+                          textColor="primary"
+                          variant="fullWidth">
+                        <Tab label="Info" {...a11yProps(0)} />
+                        <Tab label="Cast" {...a11yProps(1)} />
+                    </Tabs>
+                </Paper>
+                <TabPanel value={value} index={0}>
+                    <Box fontSize="subtitle2.fontSize"
+                         fontWeight="fontWeightBold"
+                         textAlign="right"
+                         paddingBottom={1}
+                         paddingLeft={20}>
+                        {props.details.tagline}
+                    </Box>
+                    <Box fontSize="body1.fontSize">
+                        {props.details.overview}
+                    </Box>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <List>
+                        {props.cast.map(actor => <Actor key={actor.cast_id} {...actor}/>)
+                                   .reduce((prev, curr, index) => [prev, <Divider key={index}/>, curr])
+                        }
+                    </List>
+                </TabPanel>
+            </div>
+        </React.Fragment>
     );
 };
 
