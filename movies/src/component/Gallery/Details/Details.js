15894bc64857bd6e614d588d3577d001ea2e8798
blaec <odeskonst@yandex.ru>
2021-03-17 23:33:19 +0200

    #25 add omdb query

--- movies/src/component/Gallery/Details/Details.js
+++ movies/src/component/Gallery/Details/Details.js
@@ -5,11 +5,12 @@ import BackdropImage from "./components/BackdropImage";
 import Info from "./components/Info";
 import MyLoader from "../../../UI/Spinners/MyLoader";
 import './Details.css';
-import {getMovieCreditsUrl, getMovieDetailsUrl} from "../../../utils/UrlUtils";
+import {getMovieCreditsUrl, getMovieDetailsUrl, getOmdbMovieDetails} from "../../../utils/UrlUtils";
 
 // TODO refactor multiple axios get requests
 const details = (props) => {
     const [movieDetails, setMovieDetails] = useState();
+    const [omdbMovieDetails, setOmdbMovieDetails] = useState();
     const [cast, setCast] = useState();
     const [genres, setGenres] = useState('');
     const [backdrops, setBackdrops] = useState([]);
@@ -21,12 +22,24 @@ const details = (props) => {
         setIsLoadingMovies(true);
         axios.get(getMovieDetailsUrl(props.tmdbId))
             .then(response => {
+
                 // console.log("extract data: " + (new Date()).getTime());
-                let movies = response.data;
-                setMovieDetails(movies);
-                setGenres(movies.genres.map(genre => genre.name).join(', '));
-                setBackdrops(movies.images.backdrops);
-                setIsLoadingMovies(false);
+                let details = response.data;
+                setMovieDetails(details);
+                setGenres(details.genres.map(genre => genre.name).join(', '));
+                setBackdrops(details.images.backdrops);
+
+                // Get movie additional details from omdb
+                axios.get(getOmdbMovieDetails(details.imdb_id))
+                    .then(response => {
+                        setOmdbMovieDetails(response.data);
+                        setIsLoadingMovies(false);
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        setIsLoadingMovies(false);
+                    });
+
             })
             .catch(error => {
                 console.log(error);
@@ -60,6 +73,7 @@ const details = (props) => {
                                alt={`${movieDetails.title} ${movieDetails.releaseDate}`}
                 />
                 <Info details={movieDetails}
+                      omdbDetails={omdbMovieDetails}
                       file={props}
                       cast={cast}
                       genres={genres}
