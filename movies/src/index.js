02416a84eecee95aa215800e9bcbc00423f0d49f
blaec <odeskonst@yandex.ru>
2021-05-08 11:18:00 +0300

    #32 clean index file and set logo and app name

--- movies/src/index.js
+++ movies/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {Provider} from "react-redux";
 import {createStore} from "redux";
+
+import App from './App';
+
+import registerServiceWorker from './registerServiceWorker';
 import globalReducer from "./store/reducers/global";
 
 const store = createStore(globalReducer);
