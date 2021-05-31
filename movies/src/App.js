704e38f0365685a04f923e7ce87560edacdbed23
blaec <odeskonst@yandex.ru>
2021-05-31 21:40:16 +0300

    #47 add lazy loading

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,18 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Suspense} from 'react';
 import {Redirect, Route, Switch} from "react-router-dom";
 import {useDispatch} from "react-redux";
 
 import Layout from "./hoc/Layout";
 import Collection from "./component/Pages/MenuItems/Collection/Collection";
-import FilteredCollection from "./component/Pages/PathLinks/FilteredCollection/FilteredCollection";
-import Upload from "./component/Pages/MenuItems/Upload/Upload";
-import Wishlist from "./component/Pages/MenuItems/Wishlist/Wishlist";
-import Filter from "./component/Pages/MenuItems/Filter/Filter";
-import MovieDetails from "./component/Pages/PathLinks/MovieDetails/MovieDetails";
-import ActorMovies from "./component/Pages/PathLinks/ActorMovies/ActorMovies";
+import MyLoader from "./UI/Spinners/MyLoader";
 import {reactLinks} from "./utils/UrlUtils";
 import {fetchMovies, fetchWishlist} from "./store/collection-actions";
 import {fetchConfigs} from "./store/api-actions";
+
+const Wishlist = React.lazy(() => import('./component/Pages/MenuItems/Wishlist/Wishlist'));
+const Filter = React.lazy(() => import('./component/Pages/MenuItems/Filter/Filter'));
+const Upload = React.lazy(() => import('./component/Pages/MenuItems/Upload/Upload'));
+const MovieDetails = React.lazy(() => import('./component/Pages/PathLinks/MovieDetails/MovieDetails'));
+const ActorMovies = React.lazy(() => import('./component/Pages/PathLinks/ActorMovies/ActorMovies'));
+const FilteredCollection = React.lazy(() => import('./component/Pages/PathLinks/FilteredCollection/FilteredCollection'));
 
 const app = () => {
     const {home, collection, filterByGenre, wishlist, filter, upload, movieDetails, actorMovies} = reactLinks;
@@ -27,24 +29,26 @@ const app = () => {
 
     let layout = (
         <Layout>
-            <Switch>
+            <Suspense fallback={MyLoader}>
+                <Switch>
 
-                {/* Menu items */}
-                <Route path={collection} exact component={Collection}/>
-                <Route path={wishlist} exact component={Wishlist}/>
-                <Route path={filter} exact component={Filter}/>
-                <Route path={upload} exact component={Upload}/>
+                    {/* Menu items */}
+                    <Route path={collection} exact component={Collection}/>
+                    <Route path={wishlist} exact component={Wishlist}/>
+                    <Route path={filter} exact component={Filter}/>
+                    <Route path={upload} exact component={Upload}/>
 
-                {/* Path links */}
-                <Route path={movieDetails} exact component={props => <MovieDetails {...props}/>}/>
-                <Route path={actorMovies} exact component={props => <ActorMovies {...props}/>}/>
-                <Route path={filterByGenre} exact component={props => <FilteredCollection {...props}/>}/>
+                    {/* Path links */}
+                    <Route path={movieDetails} exact component={props => <MovieDetails {...props}/>}/>
+                    <Route path={actorMovies} exact component={props => <ActorMovies {...props}/>}/>
+                    <Route path={filterByGenre} exact component={props => <FilteredCollection {...props}/>}/>
 
-                <Redirect to={home}/>
-                {/*<Redirect to='*'>*/}
-                {/*    <404_PAGE/>*/}
-                {/*</Redirect>*/}
-            </Switch>
+                    <Redirect to={home}/>
+                    {/*<Redirect to='*'>*/}
+                    {/*    <404_PAGE/>*/}
+                    {/*</Redirect>*/}
+                </Switch>
+            </Suspense>
         </Layout>
     );
 
