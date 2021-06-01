8721a679c5f2ce28cd07c8a870bae80bc42f8e03
blaec <odeskonst@yandex.ru>
2021-06-01 23:31:11 +0300

    #51 Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it.

--- movies/src/App.js
+++ movies/src/App.js
@@ -29,7 +29,7 @@ const app = () => {
 
     let layout = (
         <Layout>
-            <Suspense fallback={MyLoader}>
+            <Suspense fallback={<MyLoader/>}>
                 <Switch>
 
                     {/* Menu items */}
