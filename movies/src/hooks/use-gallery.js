bb420215437a4d32648a5e34ac8894bf762e117f
blaec <odeskonst@yandex.ru>
2021-06-04 14:04:49 +0300

    #46 connect first snackbar to central snackbar

--- movies/src/hooks/use-gallery.js
+++ movies/src/hooks/use-gallery.js
@@ -2,21 +2,21 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 
 import {isArrayExist} from "../utils/Utils";
-import {feedbackActions} from "../store/feedback-slice";
 import MyLoader from "../UI/Spinners/MyLoader";
 import Gallery from "../component/Gallery/Gallery/Gallery";
-import {useSnackbar} from "notistack";
-import {Grow, Zoom} from "@material-ui/core";
+import {feedbackActions} from "../store/feedback-slice";
 
 const useGallery = (movies) => {
     const collection = useSelector(state => state.collection[movies]);
     const dispatch = useDispatch();
-    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
     const hasMovies = isArrayExist(collection);
     useEffect(() => {
         if (hasMovies) {
-            enqueueSnackbar(`Found ${collection.length} movies`, {variant: 'info'});
+            dispatch(feedbackActions.setSnackbar({
+                message: `Found ${collection.length} movies`,
+                type: 'info'
+            }));
         }
     }, [hasMovies])
     return (
