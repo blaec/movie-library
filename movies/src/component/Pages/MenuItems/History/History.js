c26c9a7abe1924df83eedc996a7c571e6cbd6cbc
blaec <odeskonst@yandex.ru>
2021-06-11 23:29:48 +0300

    #39 added basic table

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -1,9 +1,29 @@
 import React, {useEffect} from 'react';
 import {fetchUploadHistory} from "../../../../store/upload-actions";
 import {useDispatch, useSelector} from "react-redux";
+import {makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 const history = () => {
     console.log("render");
+    const classes = useStyles();
     const uploadHistory = useSelector(state => state.upload.history);
     const dispatch = useDispatch();
 
@@ -13,9 +33,32 @@ const history = () => {
     }, []);
 
     return (
-        <div>
-            {uploadHistory.map(movie => movie.title)}
-        </div>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 };
 
