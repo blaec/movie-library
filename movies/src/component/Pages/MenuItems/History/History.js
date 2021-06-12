ef322bc343d5c527f4a06c1b2c9087938d18c673
blaec <odeskonst@yandex.ru>
2021-06-12 21:23:26 +0300

    #61 create sorted table

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -1,31 +1,202 @@
 import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
 import {useDispatch, useSelector} from "react-redux";
-
 import {fetchUploadHistory} from "../../../../store/upload-actions";
-import MyLoader from "../../../../UI/Spinners/MyLoader";
-import {isArrayExist} from "../../../../utils/Utils";
-import TablePaginationActions from "./TablePaginationActions";
+import {withStyles} from "@material-ui/core";
 
-import {
-    makeStyles,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableFooter,
-    TableHead,
-    TablePagination,
-    TableRow,
-    Typography,
-    withStyles
-} from "@material-ui/core";
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
 
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+    { id: 'title', numeric: false, disablePadding: true, label: 'Title' },
+    { id: 'type', numeric: false, disablePadding: false, label: 'Type' },
+    { id: 'size', numeric: true, disablePadding: false, label: 'Size (Gb)' },
+    { id: 'creationDate', numeric: true, disablePadding: false, label: 'Creation date' },
+];
+
+function EnhancedTableHead(props) {
+    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <StyledTableRow>
+                {headCells.map((headCell) => (
+                    <StyledTableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </StyledTableCell>
+                ))}
+            </StyledTableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    classes: PropTypes.object.isRequired,
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+    root: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(1),
+    },
+    highlight:
+        theme.palette.type === 'light'
+            ? {
+                color: theme.palette.secondary.main,
+                backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+            }
+            : {
+                color: theme.palette.text.primary,
+                backgroundColor: theme.palette.secondary.dark,
+            },
+    title: {
+        flex: '1 1 100%',
+    },
+}));
+
+const EnhancedTableToolbar = (props) => {
+    const classes = useToolbarStyles();
+    const { numSelected } = props;
+
+    return (
+        <Toolbar
+            className={clsx(classes.root, {
+                [classes.highlight]: numSelected > 0,
+            })}
+        >
+            {numSelected > 0 ? (
+                <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+                    {numSelected} selected
+                </Typography>
+            ) : (
+                <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+                    Upload history for the last 7 days:
+                </Typography>
+            )}
+
+            {numSelected > 0 ? (
+                <Tooltip title="Delete">
+                    <IconButton aria-label="delete">
+                        <DeleteIcon />
+                    </IconButton>
+                </Tooltip>
+            ) : (
+                <Tooltip title="Filter list">
+                    <IconButton aria-label="filter list">
+                        <FilterListIcon />
+                    </IconButton>
+                </Tooltip>
+            )}
+        </Toolbar>
+    );
+};
+
+EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    paper: {
+        // width: '100%',
+        margin: theme.spacing(2, 2, 2, 2),
+    },
+    table: {
+        minWidth: 750,
+    },
+    visuallyHidden: {
+        border: 0,
+        clip: 'rect(0 0 0 0)',
+        height: 1,
+        margin: -1,
+        overflow: 'hidden',
+        padding: 0,
+        position: 'absolute',
+        top: 20,
+        width: 1,
+    },
+    tableCell: {
+        margin: theme.spacing(0, 1, 0, 1),
+    },
+    container: {
+        maxHeight: 440,
+    },
+}));
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
+        // backgroundColor: theme.palette.common.black,
+        // color: theme.palette.common.white,
         fontWeight: 700,
         fontSize: 15,
     },
@@ -42,29 +213,57 @@ const StyledTableRow = withStyles((theme) => ({
     },
 }))(TableRow);
 
-const useStyles = makeStyles((theme) => ({
-    caption: {
-        padding: theme.spacing(3, 0, 1, 1),
-    },
-    table: {
-        minWidth: 650,
-    },
-    container: {
-        maxHeight: 440,
-    },
-    paper: {
-        margin: theme.spacing(0, 1, 0, 1)
-    }
-}));
-
-const history = () => {
-    const {caption, table, container, paper} = useStyles();
-
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+export default function history() {
+    const classes = useStyles();
 
     const uploadHistory = useSelector(state => state.upload.history);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUploadHistory());
+    }, []);
+
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = uploadHistory.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleClick = (event, name) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+
+        setSelected(newSelected);
+    };
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -75,70 +274,83 @@ const history = () => {
         setPage(0);
     };
 
-    useEffect(() => {
-        dispatch(fetchUploadHistory());
-    }, []);
+    const handleChangeDense = (event) => {
+        setDense(event.target.checked);
+    };
 
-    console.log(`${page * rowsPerPage} / ${page * rowsPerPage + rowsPerPage}`);
-    let historyTable = <MyLoader/>;
-    if (isArrayExist(uploadHistory)) {
-        historyTable = (
-            <Paper className={paper}>
-            <TableContainer className={container}>
-                <Table className={table} stickyHeader aria-label="sticky table">
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="left">Title</StyledTableCell>
-                            <StyledTableCell align="left">Type</StyledTableCell>
-                            <StyledTableCell align="left">Size&nbsp;(Gb)</StyledTableCell>
-                            <StyledTableCell align="left">Creation date</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {uploadHistory.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((movie) => {
-                            const {id, title, type, size, creationDate} = movie;
-                            return (
-                                <StyledTableRow key={id}>
-                                    <StyledTableCell align="left">{title}</StyledTableCell>
-                                    <StyledTableCell align="left">{type}</StyledTableCell>
-                                    <StyledTableCell align="right">{size}</StyledTableCell>
-                                    <StyledTableCell align="right">{creationDate}</StyledTableCell>
-                                </StyledTableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25, 50, 100]}
-                colSpan={3}
-                count={uploadHistory.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                    inputProps: { 'aria-label': 'rows per page' },
-                    native: true,
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-            />
-        </Paper>
-        );
-    }
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, uploadHistory.length - page * rowsPerPage);
 
     return (
-        <React.Fragment>
-            <Typography
-                className={caption}
-                variant='h6'
-                color='primary'
-            >
-                Upload history for the last 7 days:
-            </Typography>
-            {historyTable}
-        </React.Fragment>
-    );
-};
+        <div className={classes.root}>
+            <Paper className={classes.paper}>
+                <EnhancedTableToolbar numSelected={selected.length} />
+                <TableContainer className={classes.container}>
+                    <Table
+                        className={classes.table}
+                        stickyHeader
+                        aria-labelledby="tableTitle"
+                        size={dense ? 'small' : 'medium'}
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            numSelected={selected.length}
+                            order={order}
+                            orderBy={orderBy}
+                            onSelectAllClick={handleSelectAllClick}
+                            onRequestSort={handleRequestSort}
+                            rowCount={uploadHistory.length}
+                        />
+                        <TableBody>
+                            {stableSort(uploadHistory, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const isItemSelected = isSelected(row.name);
+                                    const labelId = `enhanced-table-checkbox-${index}`;
 
-export default history;
+                                    return (
+                                        <StyledTableRow
+                                            hover
+                                            onClick={(event) => handleClick(event, row.id)}
+                                            role="checkbox"
+                                            aria-checked={isItemSelected}
+                                            tabIndex={-1}
+                                            key={row.id}
+                                            selected={isItemSelected}
+                                        >
+                                            <StyledTableCell component="th" id={labelId} scope="row" padding="none">
+                                                {row.title}
+                                            </StyledTableCell>
+                                            <StyledTableCell align="right">{row.type}</StyledTableCell>
+                                            <StyledTableCell align="right">{row.size}</StyledTableCell>
+                                            <StyledTableCell align="right">{row.creationDate}</StyledTableCell>
+                                        </StyledTableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                                    <StyledTableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={uploadHistory.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+            <FormControlLabel
+                control={<Switch checked={dense} onChange={handleChangeDense} />}
+                label="Dense padding"
+            />
+        </div>
+    );
+}
