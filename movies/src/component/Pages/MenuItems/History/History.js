e33b269dcf6842a5853316366e7587cc91b8eaf7
blaec <odeskonst@yandex.ru>
2021-06-12 12:43:03 +0300

    #39 fix pagination and split logic

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -4,88 +4,23 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchUploadHistory} from "../../../../store/upload-actions";
 import MyLoader from "../../../../UI/Spinners/MyLoader";
 import {isArrayExist} from "../../../../utils/Utils";
+import TablePaginationActions from "./TablePaginationActions";
 
 import {
-    IconButton,
     makeStyles,
     Paper,
     Table,
     TableBody,
     TableCell,
-    TableContainer, TableFooter,
-    TableHead, TablePagination,
-    TableRow, Typography, useTheme,
+    TableContainer,
+    TableFooter,
+    TableHead,
+    TablePagination,
+    TableRow,
+    Typography,
     withStyles
 } from "@material-ui/core";
-import {KeyboardArrowLeft, KeyboardArrowRight} from "@material-ui/icons";
-import LastPageIcon from '@material-ui/icons/LastPage';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import PropTypes from 'prop-types';
 
-const useStyles1 = makeStyles((theme) => ({
-    root: {
-        flexShrink: 0,
-        marginLeft: theme.spacing(2.5),
-    },
-}));
-
-function TablePaginationActions(props) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
 
 const StyledTableCell = withStyles((theme) => ({
     head: {
@@ -125,15 +60,12 @@ const history = () => {
     const uploadHistory = useSelector(state => state.upload.history);
     const dispatch = useDispatch();
 
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, uploadHistory.length - page * rowsPerPage);
-
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(parseInt(event.target.value, 10));
-        console.log(parseInt(event.target.value, 10));
         setPage(0);
     };
 
@@ -141,6 +73,7 @@ const history = () => {
         dispatch(fetchUploadHistory());
     }, []);
 
+    console.log(`${page * rowsPerPage} / ${page * rowsPerPage + rowsPerPage}`);
     let historyTable = <MyLoader/>;
     if (isArrayExist(uploadHistory)) {
         historyTable = (
@@ -170,7 +103,7 @@ const history = () => {
                     <TableFooter>
                         <TableRow>
                             <TablePagination
-                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                rowsPerPageOptions={[5, 10, 25, 50, 100]}
                                 colSpan={3}
                                 count={uploadHistory.length}
                                 rowsPerPage={rowsPerPage}
