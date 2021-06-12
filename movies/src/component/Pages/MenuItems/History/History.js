b9bb94941ad7edb362d235b8bf3f3e7c60524a73
blaec <odeskonst@yandex.ru>
2021-06-12 10:46:11 +0300

    #39 fill table with basic data

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -1,7 +1,12 @@
 import React, {useEffect} from 'react';
-import {fetchUploadHistory} from "../../../../store/upload-actions";
 import {useDispatch, useSelector} from "react-redux";
+
+import {fetchUploadHistory} from "../../../../store/upload-actions";
+import MyLoader from "../../../../UI/Spinners/MyLoader";
+import {isArrayExist} from "../../../../utils/Utils";
+
 import {makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+
 
 const useStyles = makeStyles({
     table: {
@@ -9,56 +14,51 @@ const useStyles = makeStyles({
     },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 const history = () => {
-    console.log("render");
-    const classes = useStyles();
+    const {table} = useStyles();
+
     const uploadHistory = useSelector(state => state.upload.history);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        console.log("load");
         dispatch(fetchUploadHistory());
     }, []);
 
-    return (
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+    let historyTable = <MyLoader/>;
+    if (isArrayExist(uploadHistory)) {
+        historyTable = (
+            <TableContainer component={Paper}>
+                <Table className={table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Title</TableCell>
+                            <TableCell align="left">Type</TableCell>
+                            <TableCell align="left">Size&nbsp;(Gb)</TableCell>
+                            <TableCell align="left">Creation date</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {uploadHistory.map((movie) => {
+                            const {id, title, type, size, creationDate} = movie;
+                            return (
+                                <TableRow key={id}>
+                                    <TableCell align="left">{title}</TableCell>
+                                    <TableCell align="left">{type}</TableCell>
+                                    <TableCell align="right">{size}</TableCell>
+                                    <TableCell align="right">{creationDate}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    }
+
+    return (
+        <React.Fragment>
+            {historyTable}
+        </React.Fragment>
     );
 };
 
