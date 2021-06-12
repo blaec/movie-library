67016724948428cfb88a043a82f7440777c0b31b
blaec <odeskonst@yandex.ru>
2021-06-12 11:13:06 +0300

    #39 table restyling

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -5,17 +5,48 @@ import {fetchUploadHistory} from "../../../../store/upload-actions";
 import MyLoader from "../../../../UI/Spinners/MyLoader";
 import {isArrayExist} from "../../../../utils/Utils";
 
-import {makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {
+    makeStyles,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    withStyles
+} from "@material-ui/core";
 
 
-const useStyles = makeStyles({
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(3),
+    },
     table: {
         minWidth: 650,
     },
-});
+}));
 
 const history = () => {
-    const {table} = useStyles();
+    const {root, table} = useStyles();
 
     const uploadHistory = useSelector(state => state.upload.history);
     const dispatch = useDispatch();
@@ -27,26 +58,26 @@ const history = () => {
     let historyTable = <MyLoader/>;
     if (isArrayExist(uploadHistory)) {
         historyTable = (
-            <TableContainer component={Paper}>
+            <TableContainer className={root} component={Paper}>
                 <Table className={table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Title</TableCell>
-                            <TableCell align="left">Type</TableCell>
-                            <TableCell align="left">Size&nbsp;(Gb)</TableCell>
-                            <TableCell align="left">Creation date</TableCell>
+                            <StyledTableCell>Title</StyledTableCell>
+                            <StyledTableCell align="left">Type</StyledTableCell>
+                            <StyledTableCell align="left">Size&nbsp;(Gb)</StyledTableCell>
+                            <StyledTableCell align="left">Creation date</StyledTableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {uploadHistory.map((movie) => {
                             const {id, title, type, size, creationDate} = movie;
                             return (
-                                <TableRow key={id}>
-                                    <TableCell align="left">{title}</TableCell>
-                                    <TableCell align="left">{type}</TableCell>
-                                    <TableCell align="right">{size}</TableCell>
-                                    <TableCell align="right">{creationDate}</TableCell>
-                                </TableRow>
+                                <StyledTableRow key={id}>
+                                    <StyledTableCell align="left">{title}</StyledTableCell>
+                                    <StyledTableCell align="left">{type}</StyledTableCell>
+                                    <StyledTableCell align="right">{size}</StyledTableCell>
+                                    <StyledTableCell align="right">{creationDate}</StyledTableCell>
+                                </StyledTableRow>
                             );
                         })}
                     </TableBody>
@@ -57,6 +88,7 @@ const history = () => {
 
     return (
         <React.Fragment>
+            <p>Upload history for the last 7 days:</p>
             {historyTable}
         </React.Fragment>
     );
