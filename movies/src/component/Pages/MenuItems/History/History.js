89cd5295d1130d1d9291279b4bfd8c7cf0ec60d4
blaec <odeskonst@yandex.ru>
2021-06-25 09:20:54 +0300

    #66 extract sorting functions

--- movies/src/component/Pages/MenuItems/History/History.js
+++ movies/src/component/Pages/MenuItems/History/History.js
@@ -24,32 +24,7 @@ import {fetchUploadHistory} from "../../../../store/upload-actions";
 import {withStyles} from "@material-ui/core";
 import {NavLink} from "react-router-dom";
 import {reactLinks} from "../../../../utils/UrlUtils";
-
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
+import {getComparator, stableSort} from "../../../../utils/SortUtils";
 
 const headCells = [
     {id: 'title', numeric: false, disablePadding: true, label: 'Title'},
