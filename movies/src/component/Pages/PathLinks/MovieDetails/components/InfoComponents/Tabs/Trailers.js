1b4951a37e95f8dede55a3926f16f3a7d7654d10
blaec <odeskonst@yandex.ru>
2021-06-27 22:39:05 +0300

    #58 restyle trailers

--- movies/src/component/Pages/PathLinks/MovieDetails/components/InfoComponents/Tabs/Trailers.js
+++ movies/src/component/Pages/PathLinks/MovieDetails/components/InfoComponents/Tabs/Trailers.js
@@ -1,11 +1,24 @@
 import React, {useEffect} from 'react';
 import {useParams} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
+
 import {isArraysExist, isStringsExist} from "../../../../../../../utils/Utils";
 import {fetchTrailers} from "../../../../../../../store/state/details/details-actions";
 
+import {Paper} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        overflow: 'auto',
+    },
+}));
+
+
 const trailers = () => {
     const {movieTmdbId} = useParams();
+    const {root} = useStyles();
 
     const tmdbApi = useSelector(state => state.api.tmdb);
     const trailers = useSelector(state => state.details.trailers);
@@ -19,26 +32,25 @@ const trailers = () => {
 
     let trailerVideos = null;
     if (isArraysExist(trailers)) {
-        trailerVideos = trailers.map(trailer => {
-            const {key} = trailer;
+        trailerVideos = trailers.map((trailer, index) => {
+            const {key, name} = trailer;
             return (
                 <iframe
-                    width="360"
-                    height="270"
+                    key={index}
                     src={`https://www.youtube.com/embed/${key}`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
-                    title="Embedded youtube"
+                    title={name}
                 />
             )
         });
     }
 
     return (
-        <React.Fragment>
+        <Paper className={root}>
             {trailerVideos}
-        </React.Fragment>
+        </Paper>
     );
 };
 
