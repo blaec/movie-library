7655d3601d8f58d0b752e868244190fb2d3eda5f
blaec <odeskonst@yandex.ru>
2021-07-06 22:36:10 +0300

    #76 store current page

--- movies/src/store/localStorage/actions.js
+++ movies/src/store/localStorage/actions.js
@@ -3,11 +3,13 @@ const keys = Object.freeze(
         currentPage: 'current-page',
         densePadding: 'dense-padding',
         rowsPerPage: 'rows-per-page',
+        lastLocation: 'last-location',
     }
 );
 
 const globalFunctions = (key) => ({
     set: (value) => localStorage.setItem(key, value),
+    get: () => localStorage.getItem(key),
     getNumeric: () => +localStorage.getItem(key),
     getBoolean: () => localStorage.getItem(key) === 'true',
     remove: () => localStorage.removeItem(key),
@@ -29,4 +31,10 @@ export const tableRowsPerPage = {
     set: globalFunctions(keys.rowsPerPage).set,
     get: globalFunctions(keys.rowsPerPage).getNumeric,
     remove: globalFunctions(keys.rowsPerPage).remove,
+};
+
+export const lastLocation = {
+    set: globalFunctions(keys.lastLocation).set,
+    get: globalFunctions(keys.lastLocation).get,
+    remove: globalFunctions(keys.lastLocation).remove,
 };
