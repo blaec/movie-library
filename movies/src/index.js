2ba981638e5e4aeb33260f6c09f26a3864758e10
blaec <odeskonst@yandex.ru>
2021-07-23 22:28:56 +0300

    #8 Add internationalization

--- movies/src/index.js
+++ movies/src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
 
 import App from './App';
 import store from "./store/state/index";
 
 import registerServiceWorker from './registerServiceWorker';
 
+i18next.init({
+    interpolation: { escapeValue: false },  // React already does escaping
+});
+
 const app = (
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <I18nextProvider i18n={i18next}>
+                <App/>
+            </I18nextProvider>
         </BrowserRouter>
     </Provider>
 );
