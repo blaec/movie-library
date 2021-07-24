2c32ef2da842635b13fdd657a91ab771af6c13f3
blaec <odeskonst@yandex.ru>
2021-07-24 11:41:45 +0300

    #8 save current language to local storage

--- movies/src/component/Toolbar/components/LanguagePicker.js
+++ movies/src/component/Toolbar/components/LanguagePicker.js
@@ -1,25 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useTranslation} from "react-i18next";
 
 import {Button} from "@material-ui/core";
+import {language} from "../../../store/localStorage/actions";
 
 const languagePicker = () => {
-    const [lang, setLang] = useState('EN');
+    const [lang, setLang] = useState(language.get() || 'en');
     const {i18n} = useTranslation('common');
 
-    const handleChangeLanguage = () => {
-        switch (i18n.language) {
-            case 'ru':
-                setLang('EN');
-                i18n.changeLanguage('en');
-                break;
-            case 'en':
-                setLang('RU');
-                i18n.changeLanguage('ru');
-                break;
-            default:
-                console.log("error");
+    useEffect(() => {
+        if (language.get() === null) {
+            language.set('en');
         }
+        i18n.changeLanguage(language.get());
+    }, [])
+
+    const handleChangeLanguage = () => {
+        let lang;
+        switch (language.get()) {
+            case 'en':  lang = 'ru';    break;
+            default:    lang = 'en';
+        }
+        setLang(lang);
+        language.set(lang);
+        i18n.changeLanguage(lang);
     };
 
     return (
