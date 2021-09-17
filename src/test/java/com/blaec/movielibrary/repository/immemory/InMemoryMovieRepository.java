0bf7dbfee074a6cd475ba3eb8cd8f01d99ed889a
blaec <odeskonst@yandex.ru>
2021-09-17 22:30:24 +0300

    #104 implement in-memory repository and add test logs

--- src/test/java/com/blaec/movielibrary/repository/immemory/InMemoryMovieRepository.java
+++ src/test/java/com/blaec/movielibrary/repository/immemory/InMemoryMovieRepository.java
@@ -1,31 +1,58 @@
 package com.blaec.movielibrary.repository.immemory;
 
+import com.blaec.movielibrary.MovieTestData;
 import com.blaec.movielibrary.enums.Type;
 import com.blaec.movielibrary.model.Movie;
 import com.blaec.movielibrary.repository.MovieRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Repository
 public class InMemoryMovieRepository implements MovieRepository {
     Map<Integer, Movie> repository = new ConcurrentHashMap<>();
 
+    {
+        MovieTestData.WISH_MOVIES.forEach(movie -> repository.put(movie.getId(), movie));
+        MovieTestData.MOVIES.forEach(movie -> repository.put(movie.getId(), movie));
+    }
+
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("+++ PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("+++ PreDestroy");
+    }
+
     @Override
     public Iterable<Movie> getAll() {
-        return null;
+        return repository.values();
     }
 
     @Override
     public Movie getByTmdbId(String tmdbId) {
-        return null;
+        return repository.values().stream()
+                .filter(movie -> movie.getTmdbId().equals(tmdbId))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Iterable<Movie> getAllByType(Type type) {
-        return null;
+        return repository.values().stream()
+                .filter(movie -> movie.getType() == type)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -35,11 +62,12 @@ public class InMemoryMovieRepository implements MovieRepository {
 
     @Override
     public Movie save(Movie movie) {
-        return null;
+        repository.put(movie.getId(), movie);
+        return repository.get(movie.getId());
     }
 
     @Override
     public void deleteById(int id) {
-
+        repository.remove(id);
     }
 }
