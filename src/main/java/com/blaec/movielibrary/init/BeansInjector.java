8a359d43b44e44ebdcbec54b02f0a151c640f850
blaec <odeskonst@yandex.ru>
2021-10-02 22:18:04 +0300

    #104 refactoring

--- src/main/java/com/blaec/movielibrary/init/BeansInjector.java
+++ src/main/java/com/blaec/movielibrary/init/BeansInjector.java
@@ -3,15 +3,13 @@ package com.blaec.movielibrary.init;
 import com.blaec.movielibrary.utils.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-@Slf4j
 @AllArgsConstructor
-public class Init {
+public class BeansInjector {
 
     private final ObjectMapper objectMapper;
 
