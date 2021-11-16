c0a33b8903ecdd4cae74f863f7fa5d4743f8d821
blaec <odeskonst@yandex.ru>
2021-11-16 23:20:28 +0200

    #94 display posters

--- movies/src/component/Pages/PathLinks/MovieDetails/components/PosterRefreshDialog.js
+++ movies/src/component/Pages/PathLinks/MovieDetails/components/PosterRefreshDialog.js
@@ -1,11 +1,64 @@
 import React from 'react';
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    makeStyles,
+    Paper
+} from "@material-ui/core";
 import MyDialogButton from "../../../../../UI/Buttons/MyDialogButton";
 import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
+import {isArraysExist} from "../../../../../utils/Utils";
+import {getImageUrl, posterSizes} from "../../../../../utils/UrlUtils";
+
+const useStyles = makeStyles((theme) => ({
+    images: {
+        display: 'flex',
+        overflow: 'auto',
+        marginTop: theme.spacing(2),
+    },
+}));
 
 const posterRefreshDialog = (props) => {
     const {open, onExit, onDelete} = props;
     const {t} = useTranslation('common');
+    const {images} = useStyles();
+
+    const postersEn = useSelector(state => state.collection.postersEn);
+    const postersRu = useSelector(state => state.collection.postersRu);
+
+    let posterEnGallery = null;
+    if (isArraysExist(postersEn)) {
+        posterEnGallery = postersEn.map((image, index) => {
+            const {file_path} = image;
+            return (
+                <img
+                    key={index}
+                    height={250}
+                    src={getImageUrl(file_path, posterSizes.w342)}
+                    alt=''
+                />
+            )
+        });
+    }
+
+    let posterRuGallery = null;
+    if (isArraysExist(postersRu)) {
+        posterRuGallery = postersRu.map((image, index) => {
+            const {file_path} = image;
+            return (
+                <img
+                    key={index}
+                    height={250}
+                    src={getImageUrl(file_path, posterSizes.w342)}
+                    alt=''
+                />
+            )
+        });
+    }
 
     return (
         <Dialog
@@ -16,6 +69,12 @@ const posterRefreshDialog = (props) => {
                 {t('text.deleteMovie')}
             </DialogTitle>
             <DialogContent>
+                <Paper className={images} square>
+                    {posterEnGallery}
+                </Paper>
+                <Paper className={images} square>
+                    {posterRuGallery}
+                </Paper>
                 <DialogContentText>
                     {t('helperText.deleteMovieQuery')}
                 </DialogContentText>
