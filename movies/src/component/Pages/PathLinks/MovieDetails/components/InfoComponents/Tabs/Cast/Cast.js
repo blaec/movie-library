1ec3d9f4c57ec620fefcf5c8ed5b3ffb965af1b1
blaec <odeskonst@yandex.ru>
2022-01-24 21:23:55 +0200

    #122 Fix Star wars duplicate key value

--- movies/src/component/Pages/PathLinks/MovieDetails/components/InfoComponents/Tabs/Cast/Cast.js
+++ movies/src/component/Pages/PathLinks/MovieDetails/components/InfoComponents/Tabs/Cast/Cast.js
@@ -10,11 +10,12 @@ const cast = (props) => {
     const {details} = props;
     const {t} = useTranslation('common');
 
-
+    // TODO may display no-cast message before cast is loaded
     let filteredCast = details.map(actor => <Actor key={actor.id} {...actor}/>);
     let filteredCastWithDivider = filteredCast.length === 0
         ? <MyResponse message={t('text.noCastIsAvailableAtThisStage')}/>
-        : filteredCast.reduce((prev, curr) => [prev, <Divider key={Date.now()}/>, curr]);
+        : filteredCast.reduce((prev, curr, index) => [prev, <Divider key={`${index}.${curr.key}`} val={`${index}.${curr.key}`}/>, curr]);
+    console.log(filteredCast);
     return (
         <List>
             {filteredCastWithDivider}
