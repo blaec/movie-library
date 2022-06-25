d314c1a09898158536d910aa917511f00de0b98e
blaec <odeskonst@yandex.ru>
2022-06-25 19:33:45 +0300

    #131 refactoring with streams

--- src/main/java/com/blaec/movielibrary/controllers/AbstractMovieController.java
+++ src/main/java/com/blaec/movielibrary/controllers/AbstractMovieController.java
@@ -15,11 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Slf4j
 public abstract class AbstractMovieController extends AbstractController{
@@ -75,17 +75,10 @@ public abstract class AbstractMovieController extends AbstractController{
     }
 
     protected List<Response> countExistingMovies(List<MovieFileTo> folderMovies, List<MovieFileTo> newMovies) {
-        List<Response> responses = new ArrayList<>();
-        for (MovieFileTo movieFile : folderMovies) {
-            if (!newMovies.contains(movieFile)) {
-                Response existingMovie = Response.Builder.create()
-                        .setFailMessage("Already exist")
-                        .build();
-                responses.add(existingMovie);
-            }
-        }
-
-        return responses;
+        return folderMovies.stream()
+                .filter(fm -> !newMovies.contains(fm))
+                .map(fm -> Response.Builder.create().setFailMessage("Already exist").build())
+                .collect(Collectors.toList());
     }
 
     protected Predicate<MovieFileTo> isFileNameMatchRequested(SingleFileUpload uploadMovie) {
