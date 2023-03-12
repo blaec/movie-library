619952947baede47f2ad233cb238f65720129cf9
blaec <odeskonst@yandex.ru>
2023-03-12 22:57:34 +0200

    #146 allow to switch between cast/crew

--- movies/src/component/Pages/PathLinks/ActorDetails/components/MovieStatusSwitch.js
+++ movies/src/component/Pages/PathLinks/ActorDetails/components/MovieStatusSwitch.js
@@ -1,22 +1,67 @@
 import React, {useState} from 'react';
 import {useTranslation} from "react-i18next";
+import {useHistory, useParams} from "react-router";
+
+import {reactLinks} from "../../../../../utils/UrlUtils";
 
 import Switch from "@material-ui/core/Switch";
-import {makeStyles} from "@material-ui/core";
+import {FormGroup, Grid, makeStyles, Typography, withStyles} from "@material-ui/core";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
         paddingLeft: theme.spacing(5),
+    },
+    _switch: {
+        paddingTop: theme.spacing(1),
+        paddingLeft: theme.spacing(5)
     }
 }));
 
+const AntSwitch = withStyles((theme) => ({
+    root: {
+        width: 28,
+        height: 16,
+        padding: 0,
+        display: 'flex',
+    },
+    switchBase: {
+        padding: 2,
+        color: theme.palette.grey[500],
+        '&$checked': {
+            transform: 'translateX(12px)',
+            color: theme.palette.common.white,
+            '& + $track': {
+                opacity: 1,
+                backgroundColor: theme.palette.primary.main,
+                borderColor: theme.palette.primary.main,
+            },
+        },
+    },
+    thumb: {
+        width: 12,
+        height: 12,
+        boxShadow: 'none',
+    },
+    track: {
+        border: `1px solid ${theme.palette.grey[500]}`,
+        borderRadius: 16 / 2,
+        opacity: 1,
+        backgroundColor: theme.palette.common.white,
+    },
+    checked: {},
+}))(Switch);
+
 const movieStatusSwitch = (props) => {
     const {onSwitchChange} = props;
-    const {root} = useStyles();
+    const history = useHistory();
+    const params = useParams();
+    const {actorId, type} = params;
+    const {root, _switch} = useStyles();
     const [isCollectionMovie, setIsCollectionMovie] = useState(false);
     const {t} = useTranslation('common');
+    const [state, setState] = React.useState(type === 'crew');
 
     const handleCollectionMovieToggle = () => {
         setIsCollectionMovie(!isCollectionMovie);
@@ -30,12 +75,29 @@ const movieStatusSwitch = (props) => {
         />
     );
 
+    const handleChange = () => {
+        setState(!state);
+        const newType = state ? 'cast' : 'crew';
+        history.push(`${reactLinks.actorDetailsEndpoint}${actorId}/type/${newType}`)
+    };
+
     return (
-        <FormControlLabel
-            className={root}
-            control={switchControl}
-            label={t('text.movieSwitch')}
-        />
+        <FormGroup row={true}>
+            <FormControlLabel
+                className={root}
+                control={switchControl}
+                label={t('text.movieSwitch')}
+            />
+            <Typography component="div" className={_switch}>
+                <Grid component="label" container alignItems="center" spacing={1}>
+                    <Grid item>Cast</Grid>
+                    <Grid item>
+                        <AntSwitch checked={state} onChange={handleChange} />
+                    </Grid>
+                    <Grid item>Crew</Grid>
+                </Grid>
+            </Typography>
+        </FormGroup>
     );
 };
 
