21856253d24a905a7087fb925887337153ccb742
blaec <odeskonst@yandex.ru>
2023-08-20 23:20:25 +0300

    #150 connect language picker to actual translation

--- movies/src/component/Toolbar/components/LanguagePicker.js
+++ movies/src/component/Toolbar/components/LanguagePicker.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {FormControl, makeStyles, MenuItem, Select} from "@material-ui/core";
+import {language} from "../../../store/localStorage/actions";
+import {Language} from "../../../utils/Constants";
+import {useTranslation} from "react-i18next";
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -31,11 +34,39 @@ const useStyles = makeStyles((theme) => ({
 
 const LanguagePicker = () => {
     const classes = useStyles();
+    const [lang, setLang] = useState(language.get() || 'en');
     const [age, setAge] = React.useState(10);
+    const {t, i18n} = useTranslation('common');
 
     const handleChange = (event) => {
+        if (event.target.value === 10) {
+            handleSetEnglish();
+        } else if (event.target.value === 20) {
+            handleSetRussian();
+        }
         setAge(event.target.value);
     };
+    const handleSetEnglish = () => {
+        setNewLanguage(Language.english);
+    };
+
+    const handleSetRussian = () => {
+        setNewLanguage(Language.russian);
+    };
+
+    const setNewLanguage = (lang) => {
+        setLang(lang);
+        language.set(lang);
+        i18n.changeLanguage(lang);
+    };
+
+    useEffect(() => {
+        if (lang === Language.english) {
+            setAge(10);
+        } else if (lang === Language.russian) {
+            setAge(20);
+        }
+    }, [lang]);
 
 
     return (
