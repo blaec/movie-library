b233ec8535f50516ffb69b7b011ccc4e118fca12
blaec <odeskonst@yandex.ru>
2024-02-23 23:16:23 +0200

    #155 add allowed ips logic

--- src/main/java/com/blaec/movielibrary/interceptors/RequestInterceptor.java
+++ src/main/java/com/blaec/movielibrary/interceptors/RequestInterceptor.java
@@ -1,7 +1,10 @@
 package com.blaec.movielibrary.interceptors;
 
+import com.blaec.movielibrary.configs.AccessControl;
 import com.google.common.collect.ImmutableList;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,17 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Slf4j
+@AllArgsConstructor
+@Component
 public class RequestInterceptor implements HandlerInterceptor {
+    private AccessControl accessControl;
     private final List<String> methodsToLog = ImmutableList.of("POST", "DELETE", "PUT");
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String httpMethod = request.getMethod();
-        if (methodsToLog.contains(httpMethod)) {
-            log.debug("From ip {} received {} request to url: {}",
-                    request.getRemoteAddr(), httpMethod, request.getRequestURL());
+//        if (methodsToLog.contains(httpMethod)) {
+        String ip = request.getRemoteAddr();
+        log.debug("From ip {} received {} request to url: {}",
+                ip, httpMethod, request.getRequestURL());
+//        }
+
+        if (isLocal(ip) || isExternal(ip)) {
+            log.debug("Access allowed.");
+        } else {
+            log.debug("Access denied.");
         }
 
         return true;
+    }
+
+    private boolean isLocal(String ipAddress) {
+        return ipAddress.startsWith(accessControl.getLocalSubnet());
+    }
+
+    private boolean isExternal(String ipAddress) {
+        return accessControl.getExternalIps().contains(ipAddress);
     }
 }
