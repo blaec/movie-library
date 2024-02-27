f5575eab1960e890c28db94baa42fc72459ad974
blaec <odeskonst@yandex.ru>
2024-02-27 21:58:04 +0200

    #156 add health monitor

--- src/main/java/com/blaec/movielibrary/controllers/MonitorController.java
+++ src/main/java/com/blaec/movielibrary/controllers/MonitorController.java
@@ -2,6 +2,7 @@ package com.blaec.movielibrary.controllers;
 
 import com.blaec.movielibrary.utils.PermissionMonitor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,12 @@ public class MonitorController extends AbstractController {
     public static final String URL = API_VERSION + "/monitor";
 
     @GetMapping("/has-unauthorized-access")
-    public boolean getConfigs() {
-        return PermissionMonitor.isAlert();
+    public ResponseEntity<Boolean> hasUnauthorizedAccess() {
+        return ResponseEntity.ok(PermissionMonitor.isAlert());
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
     }
 }
